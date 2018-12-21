7514d6265809955de9113b917ecae8609d6793b4
eric <mahasieric@gmail.com>
2018-12-21 13:22:04 +0000

    Added TODO items. Removed useless init function. Refactored evaluate user choice method to remove redundant comments.

--- bank.py
+++ bank.py
@@ -1,13 +1,14 @@
 import account
 import sys
+# TODO create function to generate objects
+# TODO choose proper names for objects and some variables
 b = account.Account("eric", 234, 10000, 54, 256, 50)
 
 
 class Bank(object):
     """Driver class that runs the entire program."""
 
-    def __init__(self):
-        """yet to decide what variables to initialize"""
+# TODO figure out variables to be instantiated, if any necessary
 
     def display_welcome_msg(self):
         msg = 'Welcome user!\n' + '\nPlease select an action ' + \
@@ -16,24 +17,22 @@ class Bank(object):
         print(msg)
 
     def get_user_choice(self):
-        self.user_choice = int(input(" "))
+        self.user_choice = int(input())
 
     def evaluate_user_choice(self, user_choice):
         if user_choice == 1:
-            # ask for amount use setter method use getter in next line
-            b.amount = int(input("Please enter the amount you with to withdraw: "))
-            b.withdraw(b.amount)
+            b.withdraw_amount = int(input("Please enter the amount you wish to withdraw: "))
+            b.withdraw(b.withdraw_amount)
         elif user_choice == 2:
-            # ask for amount use setter method use getter in next line
-            b.amount = int(input("Please enter the amount you wish to deposit"))
-            b.deposit(b.amount)
+            b.deposit_amount = int(input("Please enter the amount you wish to deposit: "))
+            b.deposit(b.deposit_amount)
         elif user_choice == 3:
-            # use getter method to display balance
             print("You current account balance is: ", b.account_balance)
         elif user_choice == 4:
             sys.exit()
         else:
-            # throw wrong input type error
+            # TODO handle inputs outside the acceptable range of values
+            # TODO improve error handling for non-int input types
             try:
                 type(self.user_choice) != int
             except TypeError:
