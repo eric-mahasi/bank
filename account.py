e276df693ba3467dbcbe498fad5ca627bef1ba72
eric <mahasieric@gmail.com>
2018-12-21 11:55:08 +0000

    Removed useless getter and setter methods.

--- account.py
+++ account.py
@@ -12,53 +12,11 @@ class Account(object):
         self.lower_limit = lower_limit
         self.withdraw_limit = withdraw_limit
 
-
-
-    @property
-    def account_name(self):
-        return self.account_name
-
-    @account_name.setter
-    def account_name(self, account_name):
-        self.account_name = account_name
-
-    @property
-    def account_id(self):
-        return self.account_id
-
-    @account_id.setter
-    def account_id(self, account_ID):
-        self.account_id = account_ID
-
-    @property
-    def account_balance(self):
-        return self.account_balance
-
-    @account_balance.setter
-    def account_balance(self, account_balance):
-        self.account_balance = account_balance
-
-    @property
-    def withdraw_limit(self):
-        return self.withdraw_limit
-
-    @withdraw_limit.setter
-    def withdraw_limit(self, withdraw_limit):
-        self.withdraw_limit = withdraw_limit
-
-    @property
-    def amount(self):
-        return self.amount
-
-    @amount.setter
-    def amount(self, amount):
-        self.amount = amount
-
-    def display_balance(self, account_balance):
-        print(account_balance)
+    def display_balance(self):
+        print(self.account_balance)
 
     def withdraw(self, withdraw_amount):
-        self.account_balance -= self.withdraw_amount
+        self.account_balance -= withdraw_amount
 
     def deposit(self, deposit_amount):
-        self.account_balance += self.deposit_amount
+        self.account_balance += deposit_amount
