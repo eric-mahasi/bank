dc0d7d23356dcf05c97c55247208dc098b4e3f4f
eric <mahasieric@gmail.com>
2018-12-22 10:08:55 +0000

    Created function for generating objects. Added several TODO items

--- bank.py
+++ bank.py
@@ -1,14 +1,21 @@
 import account
 import sys
-# TODO create function to generate objects
-# TODO choose proper names for objects and some variables
-b = account.Account("eric", 234, 10000, 54, 256, 50)
+# TODO choose proper names for objects and some variables such as object_name to remove errors in evaluate_user_choice
+# function
 
 
 class Bank(object):
     """Driver class that runs the entire program."""
-
-# TODO figure out variables to be instantiated, if any necessary
+# TODO figure out variables to be instantiated in init function, if any necessary
+# TODO add functionality:
+# 1) Modify objects to allow parameter values to be input by user
+# 2) Allow different users to access their own accounts
+# 3) Enhance navigation through menus
+    def initialize_objects(self):
+        global eric, victor, test
+        eric = account.Account("eric", 1, 10000, 54, 256, 50)
+        victor = account.Account("victor", 2, 3500, 8493, 2097, 672)
+        test = account.Account("test", 3, 15688, 65984, 245788, 1000, 9723)
 
     def display_welcome_msg(self):
         msg = 'Welcome user!\n' + '\nPlease select an action ' + \
@@ -36,6 +43,7 @@ class Bank(object):
             print("Invalid choice. Please try again: ")
 
 
+# Objects for debugging purposes only
 a = Bank()
 a.display_welcome_msg()
 a.get_user_choice()
