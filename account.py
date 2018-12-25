3be72a1609782dfabc6cf08d0fdd89caea0e50d9
eric <mahasieric@gmail.com>
2018-12-25 10:19:44 +0000

    Cleaned up code and added one FIXME item.

--- account.py
+++ account.py
@@ -2,14 +2,13 @@
 class Account(object):
     """This class handles all the basic operations of a rudimentary bank account."""
 
-    def __init__(self, account_name, account_id, account_balance,
-                 deposit_amount, withdraw_amount, withdraw_limit, lower_limit=10000):
+    def __init__(self, account_name, account_id, account_balance, deposit_amount, withdraw_amount, withdraw_limit):
         self.account_name = account_name
         self.account_id = account_id
         self.account_balance = account_balance
         self.deposit_amount = deposit_amount
         self.withdraw_amount = withdraw_amount
-        self.lower_limit = lower_limit
+        self.lower_limit = 1000
         self.withdraw_limit = withdraw_limit
 
     def display_balance(self):
--- bank.py
+++ bank.py
@@ -1,21 +1,26 @@
 import account
 import sys
 # TODO choose proper names for objects and some variables such as object_name to remove errors in evaluate_user_choice
-# function
+
+
+def initialize_objects():
+    global eric, victor, test
+    eric = account.Account("eric", 1, 10000, 54, 256, 50)
+    victor = account.Account("victor", 2, 3500, 8493, 2097, 672)
+    test = account.Account("test", 3, 15688, 65984, 245788, 1000)
 
 
 class Bank(object):
     """Driver class that runs the entire program."""
 # TODO figure out variables to be instantiated in init function, if any necessary
 # TODO add functionality:
-# 1) Modify objects to allow parameter values to be input by user
-# 2) Allow different users to access their own accounts
+# 1) Modify objects to allow parameter values to be input by user such that each user creates a different object
+# 2) Allow different users to access their own different accounts
 # 3) Enhance navigation through menus
-    def initialize_objects(self):
-        global eric, victor, test
-        eric = account.Account("eric", 1, 10000, 54, 256, 50)
-        victor = account.Account("victor", 2, 3500, 8493, 2097, 672)
-        test = account.Account("test", 3, 15688, 65984, 245788, 1000, 9723)
+
+    def __init__(self):
+        self.user_choice = 0
+        pass
 
     def display_welcome_msg(self):
         msg = 'Welcome user!\n' + '\nPlease select an action ' + \
@@ -26,15 +31,16 @@ class Bank(object):
     def get_user_choice(self):
         self.user_choice = int(input())
 
+# FIXME unresolved attributes
     def evaluate_user_choice(self, user_choice):
         if user_choice == 1:
-            b.withdraw_amount = int(input("Please enter the amount you wish to withdraw: "))
-            b.withdraw(b.withdraw_amount)
+            self.withdraw_amount = int(input("Please enter the amount you wish to withdraw: "))
+            self.withdraw(self.withdraw_amount)
         elif user_choice == 2:
-            b.deposit_amount = int(input("Please enter the amount you wish to deposit: "))
-            b.deposit(b.deposit_amount)
+            self.deposit_amount = int(input("Please enter the amount you wish to deposit: "))
+            self.deposit(self.deposit_amount)
         elif user_choice == 3:
-            print("You current account balance is: ", b.display_balance())
+            print("You current account balance is: ", self.display_balance())
         elif user_choice == 4:
             print("Quitting... ")
             sys.exit()
