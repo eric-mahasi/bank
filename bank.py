8a3aae992197f9c60fb8bbdfab32b3e7bc525520
eric <mahasieric@gmail.com>
2018-12-26 10:13:11 +0000

    Implemented several TODO items; instantiated the required variables in init function, added a function for creating accounts, fixed unresolved attributes in evaluate user choice method. Added one FIXME and one TODO item.

--- bank.py
+++ bank.py
@@ -1,8 +1,8 @@
 import account
 import sys
-# TODO choose proper names for objects and some variables such as object_name to remove errors in evaluate_user_choice
 
 
+# FIXME probably useless function?
 def initialize_objects():
     global eric, victor, test
     eric = account.Account("eric", 1, 10000, 54, 256, 50)
@@ -12,11 +12,17 @@ def initialize_objects():
 
 class Bank(object):
     """Driver class that runs the entire program."""
-# TODO figure out variables to be instantiated in init function, if any necessary
     def __init__(self):
         self.user_choice = 0
-        pass
-
+        self.account_name = ""
+        self.account_id = 0
+        self.deposit_amount = 0
+        self.account_balance = 0
+        self.withdraw_amount = 0
+        self.withdraw_limit = 0
+        self.object_name = account.Account(self.account_name, self.account_id, self.account_balance,
+                                           self.deposit_amount, self.withdraw_amount, self.withdraw_limit)
+    # FIXME deal with static method
     def display_welcome_msg(self):
         msg = 'Welcome user!\n' + '\nPlease select an action ' + \
               '\n1---Withdraw' + '\n2---Deposit' + '\n3---Check balance' + \
@@ -24,27 +30,44 @@ class Bank(object):
         return msg
 
     # TODO add functionality:
-    # 1) Modify objects to allow parameter values to be input by user such that each user creates a different object;
-    # account creation
-    # 2) Allow different users to access their own different accounts; login verification
-    # 3) Enhance navigation through menus
+    # 1) Allow different users to access their own different accounts
+    # 2) Enhance navigation through menus
     def nav_menu(self):
-        """Allows users to navigate through the several menus"""
-        print("Do you have an account? ")
+        """Allows users to navigate through the several menus."""
+        print("Welcome user... ")
+        n = input("Do you have an account? Y/N")
+        if n.lower() == "y":
+            self.verify_login()
+        elif n.lower() == "n":
+            print("Please wait while we create an account for you... ")
+            self.create_account()
+
+    def create_account(self):
+        """Create an account, passing user input into the parameters for Account objects. Returns an Account object."""
+        self.account_name = input("Account name: ")
+        self.account_id = int(input("Account ID: "))
+        self.account_balance = int(input("Account balance: "))
+        self.object_name = account.Account(self.account_name, self.account_id, self.account_balance,
+                                           self.deposit_amount, self.withdraw_amount, self.withdraw_limit)
+        return self.object_name
+
+    def verify_login(self):
+        """Check if account name and account id provided by user match those stored in file."""
+        # TODO improve security by adding password functionality to user accounts
+        pass
 
     def get_user_choice(self):
         self.user_choice = int(input())
 
-# FIXME unresolved attributes
     def evaluate_user_choice(self, user_choice):
         if user_choice == 1:
             self.withdraw_amount = int(input("Please enter the amount you wish to withdraw: "))
-            self.withdraw(self.withdraw_amount)
+            self.object_name.withdraw(self.withdraw_amount)
         elif user_choice == 2:
             self.deposit_amount = int(input("Please enter the amount you wish to deposit: "))
-            self.deposit(self.deposit_amount)
+            self.object_name.deposit(self.deposit_amount)
         elif user_choice == 3:
-            print("You current account balance is: ", self.display_balance())
+            print("You current account balance is: ", self.object_name.display_balance())
         elif user_choice == 4:
             print("Quitting... ")
             sys.exit()
@@ -55,6 +78,7 @@ class Bank(object):
 
 # Objects for debugging purposes only
 a = Bank()
+a.nav_menu()
 a.display_welcome_msg()
 a.get_user_choice()
 a.evaluate_user_choice(a.user_choice)
