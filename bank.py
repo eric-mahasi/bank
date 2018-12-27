4855456a21fd86faffa69f875925e92d859cffa8
eric <mahasieric@gmail.com>
2018-12-27 09:39:06 +0000

    Cleaned up code.

--- bank.py
+++ bank.py
@@ -4,6 +4,7 @@ import sys
 
 class Bank(object):
     """Driver class that runs the entire program."""
+
     def __init__(self):
         self.user_choice = 0
         self.account_name = ""
@@ -12,59 +13,60 @@ class Bank(object):
         self.account_balance = 0
         self.withdraw_amount = 0
         self.withdraw_limit = 0
-        self.msg = ""
+        self.menu_msg = ""
         self.object_name = account.Account(self.account_name, self.account_id, self.account_balance,
                                            self.deposit_amount, self.withdraw_amount, self.withdraw_limit)
 
-    def display_welcome_msg(self):  # TODO rename this function to something more suiting
-        self.msg = "\nPlease select an action " + "\n1---Withdraw" + "\n2---Deposit" + "\n3---Check balance" + \
-                   "\n4---Exit"
-        print(self.msg)
+    def show_main_menu(self):
+        self.menu_msg = "\nPlease select an action " + "\n1---Withdraw" + "\n2---Deposit" + "\n3---Check balance" + \
+                        "\n4---Exit"
+        print(self.menu_msg)
 
     # TODO add functionality:
     # 1) Allow different users to access their own different accounts
-    # 2) Enhance navigation through menus
-    def nav_menu(self):
+    # 2) Enhance navigation through menus. Make it possible for users to fluidly move through all the different menu
+    # screens
+    def navigate_menu(self):
         """Allows users to navigate through the several menus."""
         print("Welcome user... ")
         n = input("Do you have an account? Y/N ")  # TODO choose more descriptive variable name
         if n.lower() == "y":
             self.verify_login()
         elif n.lower() == "n":
-            print("Please be patient while we set up an account for you... ")
             self.create_account()
-            print("\nAccount creation successful. Welcome " + str(self.object_name.account_name.title()) + "\n")
         else:
-            print("Invalid choice. Please try again")
+            print("Invalid choice. Please try again.")
 
-# TODO provide much more informative print statements; clarify output
     def create_account(self):
         """Create an account, passing user input into the parameters for Account objects. Returns an Account object."""
+        print("Please be patient while we set up an account for you... ")
+        print("\nKindly enter the appropriate values after each prompt below. ")
         self.account_name = input("Account name: ")
         self.account_id = int(input("Account ID: "))
         self.account_balance = int(input("Account balance: "))
         self.object_name = account.Account(self.account_name, self.account_id, self.account_balance,
                                            self.deposit_amount, self.withdraw_amount, self.withdraw_limit)
+        print("\nAccount creation successful. Welcome " + str(self.object_name.account_name.title()) + ".")
         return self.object_name
 
     def verify_login(self):
         """Check if account name and account id provided by user match those stored in file."""
-        # TODO improve security by adding password functionality to user accounts
+        # TODO improve security by adding password attribute to user accounts
         pass
 
     def get_user_choice(self):  # FIXME probably no point in this being a function
         self.user_choice = int(input())
 
-    def evaluate_user_choice(self, user_choice):
-        if user_choice == 1:
+    def evaluate_user_choice(self):
+        if self.user_choice == 1:
             self.withdraw_amount = int(input("Please enter the amount you wish to withdraw: "))
             self.object_name.withdraw(self.withdraw_amount)
-        elif user_choice == 2:
+        elif self.user_choice == 2:
             self.deposit_amount = int(input("Please enter the amount you wish to deposit: "))
             self.object_name.deposit(self.deposit_amount)
-        elif user_choice == 3:
+        elif self.user_choice == 3:
             print("You current account balance is: ", self.object_name.display_balance())
-        elif user_choice == 4:
+        elif self.user_choice == 4:
             print("Quitting... ")
             sys.exit()
         else:
@@ -74,7 +76,7 @@ class Bank(object):
 
 # Objects for debugging purposes only
 a = Bank()
-a.nav_menu()
-a.display_welcome_msg()
+a.navigate_menu()
+a.show_main_menu()
 a.get_user_choice()
-a.evaluate_user_choice(a.user_choice)
+a.evaluate_user_choice()
