35299cb54d6f056ec3ede5710844f1aa0a85cc69
eric <mahasieric@gmail.com>
2019-01-27 20:28:17 +0300

    Major restructure of code. Moved all the logic concerned with handling any user account need into the account class.

--- account.py
+++ account.py
@@ -1,13 +1,20 @@
 class Account(object):
     """This class handles all the basic operations of a rudimentary bank account."""
 
-    def __init__(self, account_name, account_id, deposit_amount, withdraw_amount, account_balance=1000):
+    def __init__(self):
+        self.user_choice = int(input())
         self.lower_limit = 1000
-        self.account_name = account_name
-        self.account_id = account_id
-        self.account_balance = account_balance
-        self.deposit_amount = deposit_amount
-        self.withdraw_amount = withdraw_amount
+        self.account_name = ""
+        self.account_id = 0
+        self.account_balance = 1000
+        self.deposit_amount = 0
+        self.withdraw_amount = 0
+        self.account_pin = 0
+        self.confirm_account_pin = 0
+        self.object_name = None
+
+    # FIXME bug in this method prevents withdrawal
+    # FIXME account balance takes the value of 0
 
     def withdraw(self):
         self.withdraw_amount = int(input("Please enter the amount you wish to withdraw: "))
@@ -24,4 +31,71 @@ class Account(object):
         self.account_balance += self.deposit_amount
         print("Transaction successful. Your new account balance is", self.account_balance)
 
-    # FIXME account balance takes the value of 0
+    def log_in_menu(self):
+        """Allows users to navigate through the several menus."""
+        print("Welcome...")
+        print("\nPlease select an action " + "\n1---Log into my account" + "\n2---Create new account")
+        if self.user_choice == 1:
+            self.verify_login()
+        elif self.user_choice == 2:
+            self.create_account()
+        else:
+            print("Invalid choice. Please try again.")
+            self.log_in_menu()
+
+    def create_account(self):
+        """Create an account, passing user input into the parameters for Account objects. Returns an Account object."""
+        print("Please be patient while we set up an account for you... ")
+        print("\nKindly enter the appropriate information after each prompt below. ")
+        self.account_name = input("Account name: ")
+        # self.account_id = int(input("Account ID: "))  # TODO generate account ID sequentially from already existing
+        # accounts
+        while True:
+            self.account_balance = int(input("Initial deposit amount: "))
+            if self.account_balance <= self.lower_limit:
+                print("Account balances lower than", self.lower_limit, "are not allowed. Please try again.")
+            else:
+                break
+        self.confirm_pin()
+        self.object_name = Account(self.account_name, self.account_id, self.account_balance,
+                                   self.deposit_amount, self.withdraw_amount)
+        print("\nAccount creation successful. Welcome " + str(self.object_name.account_name.title()) + ".")
+        return self.object_name
+
+    def verify_login(self):
+        """Check if account name and account id provided by user match those stored in file."""
+        print("Please be patient while we verify your details...")
+        print("\nKindly enter the appropriate values after each prompt below. ")
+        self.account_name = input("Account name: ")
+        self.account_id = input("Account ID: ")
+        self.account_pin = input("Account PIN: ")
+        # self.confirm_pin()
+        # TODO compare these values with those stored in the file
+        # TODO figure out how to handle storing and accessing user account details in the file
+
+        # FIXME PIN functionality works for only one iteration instead of continually till the required conditions
+        # are met
+
+    def confirm_pin(self):
+        """Handles a 4-digit number that will be used as a PIN for accessing user accounts. Ensures that the PIN is
+         entered correctly by having the user enter it twice and the two entries are then compared to each other.
+         Returns PIN."""
+        while True:
+            self.account_pin = input("Account PIN: ")
+            # FIXME these if loops runs infinitely
+            if len(str(self.account_pin)) < 4:
+                print("PIN must be at least four digits.")
+                break
+            else:
+                self.confirm_account_pin = input("Please enter PIN again: ")
+                while True:
+                    if self.account_pin == self.confirm_account_pin:
+                        print("PIN successfully recorded.")
+                    else:
+                        print("PINs do not match. Please try again.")
+                        break
+                break
+        return self.account_pin
+
+    # TODO add functionality:
+    # 1) Allow different users to access their own different accounts
--- bank.py
+++ bank.py
@@ -1,5 +1,4 @@
-import account
-import sys
+from account import *
 
 
 class Bank(object):
@@ -16,8 +15,7 @@ class Bank(object):
         self.account_pin = 0
         self.account_pin_confirm = 0
         self.menu_msg = ""
-        self.object_name = account.Account(self.account_name, self.account_id, self.account_balance,
-                                           self.deposit_amount, self.withdraw_amount)
+        self.object_name = None
 
     def show_main_menu(self):
         self.menu_msg = "\nPlease select an action " + "\n1---Withdraw" + "\n2---Deposit" + "\n3---Check balance" + \
@@ -31,86 +29,21 @@ class Bank(object):
         elif self.user_choice == 3:
             print("Your current account balance is: ", self.object_name.account_balance)
         elif self.user_choice == 4:
-            self.log_in_menu()
+            self.object_name.log_in_menu()
         elif self.user_choice == 5:
             print("Quitting... ")
-            sys.exit()
+            quit()
         else:
             # TODO Handle errors for non-int inputs maybe in unit tests
             print("Invalid choice. Please try again: ")
         self.show_main_menu()
 
-    # TODO add functionality:
-    # 1) Allow different users to access their own different accounts
-    # TODO move all the following methods to account class
-    def log_in_menu(self):
-        """Allows users to navigate through the several menus."""
-        print("Welcome...")
-        print("\nPlease select an action " + "\n1---Log into my account" + "\n2---Create new account")
-        self.user_choice = int(input())
-        if self.user_choice == 1:
-            self.verify_login()
-        elif self.user_choice == 2:
-            self.create_account()
-        else:
-            print("Invalid choice. Please try again.")
-            self.log_in_menu()
-
-    def create_account(self):
-        """Create an account, passing user input into the parameters for Account objects. Returns an Account object."""
-        print("Please be patient while we set up an account for you... ")
-        print("\nKindly enter the appropriate information after each prompt below. ")
-        self.account_name = input("Account name: ")
-        # self.account_id = int(input("Account ID: "))  # TODO generate account ID sequentially from already existing
-        # accounts
-        while True:
-            self.account_balance = int(input("Initial deposit amount: "))
-            if self.account_balance <= self.lower_limit:
-                print("Account balances lower than", self.lower_limit, "are not allowed. Please try again.")
-            else:
-                break
-        self.confirm_pin()
-        self.object_name = account.Account(self.account_name, self.account_id, self.account_balance,
-                                           self.deposit_amount, self.withdraw_amount)
-        print("\nAccount creation successful. Welcome " + str(self.object_name.account_name.title()) + ".")
-        return self.object_name
-
-    # FIXME PIN functionality works for only one iteration instead of continually till the required conditions
-    # are met
-    def confirm_pin(self):
-        """Handles a 4-digit number that will be used as a PIN for accessing user accounts. Ensures that the PIN is
-         entered correctly by having the user enter it twice and the two entries are then compared to each other.
-         Returns PIN."""
-        while True:
-            self.account_pin = input("Account PIN: ")
-            # FIXME these if loops runs infinitely
-            if len(str(self.account_pin)) < 4:
-                print("PIN must be at least four digits.")
-                break
-            else:
-                self.account_pin_confirm = input("Please enter PIN again: ")
-                while True:
-                    if self.account_pin == self.account_pin_confirm:
-                        print("PIN successfully recorded.")
-                    else:
-                        print("PINs do not match. Please try again.")
-                        break
-                break
-        return self.account_pin
-
-    def verify_login(self):
-        """Check if account name and account id provided by user match those stored in file."""
-        print("Please be patient while we verify your details...")
-        print("\nKindly enter the appropriate values after each prompt below. ")
-        self.account_name = input("Account name: ")
-        self.account_id = input("Account ID: ")
-        self.account_pin = input("Account PIN: ")
-        # self.confirm_pin()
-        # TODO compare these values with those stored in the file
-        # TODO figure out how to handle storing and accessing user account details in the file
-
 
 # Objects for debugging purposes only
 a = Bank()
-a.log_in_menu()
+
+b = Account()
+b.deposit()
+print(a.show_main_menu())
+print(b.log_in_menu())
 a.show_main_menu()
