a4139b2ee5d0e6d1eb44ff4101df6090779f60c1
Victor Otieno Omondi <vickz84259@gmail.com>
2019-01-30 00:19:17 +0300

    Refactoring Account class
    Having deposit_amount and withdraw_amount as instance variables is
    unnecessary.  They are better off as variables local to their
    respective functions.
    
    Also since the variable lower_limit is common to all classes, having
    it as a class variable makes more sense. Such that if you want to
    change the limit at run time you just need to change it once and it'll
    reflect in all objects of that class
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- account.py
+++ account.py
@@ -3,35 +3,41 @@ class Account(object):
     This class handles all the basic operations of a rudimentary bank
     account.
     """
+    LOWER_LIMIT = 1000
 
-    def __init__(
-            self, account_name, account_id, deposit_amount,
-            withdraw_amount, account_balance=1000):
-        self.lower_limit = 1000
+    def __init__(self, account_name, account_id, account_balance=1000):
         self.account_name = account_name
         self.account_id = account_id
         self.account_balance = account_balance
-        self.deposit_amount = deposit_amount
-        self.withdraw_amount = withdraw_amount
 
+    # Suggestion: Instead of having this function carry out the logic of
+    # getting user input, why not have this functionality in the bank Class
+    # then pass the value to this function and process the value from there.
+
+    # This function could throw an excepion if the withdraw_amount is greater
+    # than the account_balance.
     def withdraw(self):
         prompt = "Please enter the amount you wish to withdraw: "
-        self.withdraw_amount = int(input(prompt))
-        if self.withdraw_amount >= self.account_balance:
+        withdraw_amount = int(input(prompt))
+        if withdraw_amount >= self.account_balance:
             print(
                 "Prohibited transaction. Account balances lower than "
-                f"{self.lower_limit} are not allowed. Please try again.")
+                f"{self.LOWER_LIMIT} are not allowed. Please try again.")
         else:
             # TODO prevent account balances lower than lower limit
-            self.account_balance -= self.withdraw_amount
+            self.account_balance -= withdraw_amount
             print(
                 "Transaction successful. Your new account balance is "
                 f"{self.account_balance}")
 
+    # Suggestion: Instead of having this function carry out the logic of
+    # getting user input, why not have this functionality in the bank Class
+    # then pass the value to this function and process the value from there.
     def deposit(self):
         prompt = "Please enter the amount you wish to deposit: "
-        self.deposit_amount = int(input(prompt))
-        self.account_balance += self.deposit_amount
+        deposit_amount = int(input(prompt))
+
+        self.account_balance += deposit_amount
         print("Transaction successful. Your new account balance is",
               self.account_balance)
 
