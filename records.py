e6d221702136e6603a2a9de0cbfadbd78511012b
eric <mahasieric@gmail.com>
2019-02-22 18:42:32 +0300

    Added feature that allows reading and writing to a csv file.

--- records.py
+++ records.py
@@ -1,14 +1,31 @@
-class Records(object):
-    """This class handles all the operations pertaining to user records."""
+import csv
+
+
+class Record(object):
+    """This class stores and accesses stored user account information from a
+    csv
+    file.
+    """
 # TODO increase security by encrypting file
 # TODO improve security by encrypting passwords
 
-    def __init__(self, file_name="user_records.csv"):
-        self.file_name = file_name
+    def __init__(self):
+        self.file_name = "records.csv"
 
-    def write_to_file(self, file_name):
-        with open(self.file_name, 'a') as file_object:
-            file_object.write("placeholder user data")
+    def write_to_file(self, account_dict):
+        """Stores user account information to a file.
+
+        Args:
+            account_dict: A dictionary containing some of the user account
+            details.
+        """
+        with open(self.file_name, mode='a') as record_file:
+            fieldnames = ['name', 'id', 'pin', 'balance']
+            record_writer = csv.DictWriter(record_file, fieldnames=fieldnames)
+            record_writer.writerow(account_dict)
 
     def read_from_file(self):
-        pass
+        with open(self.file_name) as record_file:
+            record_reader = csv.reader(record_file, delimiter=',')
+            for row in record_reader:
+                pass
