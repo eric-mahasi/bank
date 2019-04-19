44753cedecd3a52684a4e1ea38b52b823a18c1bc
Eric Mahasi <mahasieric@gmail.com>
2019-04-19 18:27:50 +0300

    Add explanatory comment
    Signed-off-by: Eric Mahasi <mahasieric@gmail.com>

--- bank.py
+++ bank.py
@@ -127,7 +127,8 @@ class Bank(object):
         print("\nKindly enter the appropriate information after each prompt"
               " below. ")
         account_name = input("Account name: ")
-
+        # Count the number of lines in the file, which is the number of
+        # existing user accounts.
         num_lines = sum(1 for line in open(self.file_name))
         account_id = num_lines + 1
 
